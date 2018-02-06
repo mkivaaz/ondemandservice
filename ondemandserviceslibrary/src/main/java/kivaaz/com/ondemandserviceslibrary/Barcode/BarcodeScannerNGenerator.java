3516ab0f96c595dff4b6bcbd48604cf60f140878
mkivaaz <mkivaaz@gmail.com>
2018-02-06 19:04:20 +0900

    BarcodeStuff

---
+++ ondemandserviceslibrary/src/main/java/kivaaz/com/ondemandserviceslibrary/Barcode/BarcodeScannerNGenerator.java
@@ -0,0 +1,82 @@
+package kivaaz.com.ondemandserviceslibrary.Barcode;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Vibrator;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.SparseArray;
+import android.view.SurfaceView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import kivaaz.com.ondemandserviceslibrary.FirebaseChat.FirebaseConstants;
+
+/**
+ * Created by Muguntan on 2/6/2018.
+ */
+
+public class BarcodeScannerNGenerator {
+    SurfaceView cameraView;
+    CameraSource cameraSource;
+    BarcodeDetector detector;
+    Context context;
+    static String qrCodeData = "";
+    public BarcodeScannerNGenerator(SurfaceView cameraView, CameraSource cameraSource, Context context) {
+        this.cameraView = cameraView;
+        this.cameraSource = cameraSource;
+        this.context = context;
+    }
+
+    public void setDetector(){
+        detector = new BarcodeDetector.Builder(context)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        detector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if(qrCodes.size() != 0){
+                    Vibrator vibrator = (Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
+                    if(!qrCodes.valueAt(0).displayValue.equals(qrCodeData)){
+                        vibrator.vibrate(1000);
+                        qrCodeData = qrCodes.valueAt(0).displayValue;
+                        Intent intent = new Intent(FirebaseConstants.BARCODE_SCANNER_INTENT);
+                        intent.putExtra(FirebaseConstants.BARCODE_DATA, qrCodeData);
+                        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+                    }
+                }
+            }
+        });
+    }
+
+
+
+    public Bitmap generateQRCode(String qrData){
+        MultiFormatWriter writer = new MultiFormatWriter();
+        try {
+            BitMatrix matrix = writer.encode(qrData, BarcodeFormat.QR_CODE,1000,1000);
+            BarcodeEncoder encoder = new BarcodeEncoder();
+            Bitmap bitmap = encoder.createBitmap(matrix);
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- ondemandserviceslibrary/src/main/java/kivaaz/com/ondemandserviceslibrary/FirebaseChat/FirebaseConstants.java
+++ ondemandserviceslibrary/src/main/java/kivaaz/com/ondemandserviceslibrary/FirebaseChat/FirebaseConstants.java
@@ -8,8 +8,10 @@ public class FirebaseConstants {
     public final static String FIREBASE_RECEIVER_ID_TAG = "Receiver_ID";
     public final static String FIREBASE_CHAT_GROUP_TAG = "ChatGroup_ID";
     public final static String FIREBASE_CHAT_INTENT = "kivaaz.com..FirebaseMessage";
+    public final static String BARCODE_SCANNER_INTENT = "kivaaz.com..BarcodeScanner";
 
     public final static String FIREBASE_MESSAGE_TAG = "Chat_Message";
+    public final static String BARCODE_DATA = "Barcode_Data";
     public final static String FIREBASE_MESSAGE_SENDER_TAG = "Chat_Sender";
     public final static String FIREBASE_MESSAGE_TIMESTAMP_TAG = "Chat_TimeStamp";
     public final static String FIREBASE_MESSAGE_ID_TAG = "Chat_Message_Id";
