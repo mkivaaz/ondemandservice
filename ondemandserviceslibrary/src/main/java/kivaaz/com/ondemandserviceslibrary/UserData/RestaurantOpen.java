002a7dfe4df81b0e0861b4227f7ff2dbb39cb3b7
mkivaaz <mkivaaz@gmail.com>
2018-03-04 20:57:57 +0900

    RestaurantOpen

--- ondemandserviceslibrary/src/main/java/kivaaz/com/ondemandserviceslibrary/UserData/RestaurantOpen.java
+++ ondemandserviceslibrary/src/main/java/kivaaz/com/ondemandserviceslibrary/UserData/RestaurantOpen.java
@@ -10,7 +10,12 @@ public class RestaurantOpen {
     String restaurant_Name;
     String restaurant_Address;
     String restaurant_Id;
-
+    String seller_email;
+    List<String> category;
+    String delivery_Time;
+    String promotion;
+    Float ratings;
+    String restaurant_ImgURl;
     String g;
     List<Double> l;
 
@@ -38,6 +43,54 @@ public class RestaurantOpen {
 
     public void setRestaurant_Address(String restaurant_Address) {
         this.restaurant_Address = restaurant_Address;
+    }
+
+    public String getSeller_email() {
+        return seller_email;
+    }
+
+    public void setSeller_email(String seller_email) {
+        this.seller_email = seller_email;
+    }
+
+    public List<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<String> category) {
+        this.category = category;
+    }
+
+    public String getDelivery_Time() {
+        return delivery_Time;
+    }
+
+    public void setDelivery_Time(String delivery_Time) {
+        this.delivery_Time = delivery_Time;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(String promotion) {
+        this.promotion = promotion;
+    }
+
+    public Float getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Float ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getRestaurant_ImgURl() {
+        return restaurant_ImgURl;
+    }
+
+    public void setRestaurant_ImgURl(String restaurant_ImgURl) {
+        this.restaurant_ImgURl = restaurant_ImgURl;
     }
 
     public String getRestaurant_Id() {
